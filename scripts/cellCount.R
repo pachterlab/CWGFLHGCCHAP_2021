f0f28ff51d803b683854d6be1c5bd5cd271fb40a
tarachari3 <tarachari3@gmail.com>
2021-06-25 10:56:39 -0700

    Update cellCount.R

--- scripts/cellCount.R
+++ scripts/cellCount.R
@@ -11,3 +11,15 @@ p + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.8) +
   ylab("Cells/ml (x10E4)") + 
   xlab("Condition") +
   ggtitle("Cells per Organism")
+
+#All pairwise fold-changes, get std dev
+control <- c(47,51,50,55,59,60,67,57)
+starved <- c(26,27,22,21,23,25,23,26)
+
+f1 <- function(x, y) list(x/y)
+out1 <- outer(control, starved, FUN = Vectorize(f1)) 
+
+mean(as.numeric(out1))
+
+sd(as.numeric(out1))
+
