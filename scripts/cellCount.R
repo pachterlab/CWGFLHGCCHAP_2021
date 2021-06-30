82142f6c04d42f46c2bbd6e16e91459c1182ad61
tarachari3 <tarachari3@gmail.com>
2021-06-30 10:47:55 -0700

    Update cellCount.R

--- scripts/cellCount.R
+++ scripts/cellCount.R
@@ -1,20 +1,20 @@
 library(ggplot2)
 
 forPlot <- data.frame(
-  y = c(47,51,50,55,59,60,67,57,26,27,22,21,23,25,23,26),
+  y = c(23.5,25.5,25,27.5,29.5,30,33.5,28.5,13,13.5,11,10.5,11.5,12.5,11.5,13),
   x = as.factor(c(rep('Control',8),rep('Starved',8)))
 )
 
 p <- ggplot(forPlot, aes(x=x, y=y)) + geom_boxplot()
 
 p + geom_dotplot(binaxis='y', stackdir='center', dotsize=0.8) +
-  ylab("Cells/ml (x10E4)") + 
+  ylab("Cells/animal (x10E4)") + 
   xlab("Condition") +
-  ggtitle("Cells per Organism")
+  ggtitle("Cells per Animal")
 
 #All pairwise fold-changes, get std dev
-control <- c(47,51,50,55,59,60,67,57)
-starved <- c(26,27,22,21,23,25,23,26)
+control <- c(23.5,25.5,25,27.5,29.5,30,33.5,28.5)
+starved <- c(13,13.5,11,10.5,11.5,12.5,11.5,13)
 
 f1 <- function(x, y) list(x/y)
 out1 <- outer(control, starved, FUN = Vectorize(f1)) 
@@ -22,4 +22,3 @@ out1 <- outer(control, starved, FUN = Vectorize(f1))
 mean(as.numeric(out1))
 
 sd(as.numeric(out1))
-
